3a189c16ad8f923ad5e51e57986dd0f097e19482
Reinhard Lenz <71219487+ReinhardLenz@users.noreply.github.com>
2025-04-23 09:43:13 +0300

    Update locators.py
    Scraped website has changed, modified locators

--- locators.py
+++ locators.py
@@ -1,48 +1,125 @@
 class Locators():
     # Login page objects
     cookie_acceptor= ".js-bsg-cookie-layer__confirm > span:nth-child(1)"
+
+
     cookie_acceptor_class="bsg-cookie-layer__btn-confirm bsg-btn bsg-btn--cara bsg-btn--centered js-bsg-cookie-layer__confirm"
-    cookie_acceptor_full_XP="/html/body/aside/div[2]/div[2]/button[1]"
+    #cookie_acceptor_full_XP="/html/body/aside/div[2]/div[2]/button[1]"
+    #cookie_acceptor_full_XP="/html/body/div[1]/aside/div[2]/div[3]/button[1]/i"
+    cookie_acceptor_full_XP="/html/body/div[1]/aside/div[2]/div[3]/button[1]"
+
+    
+    Cookie_einstellungen_fullXPATH="/html/body/div[3]/aside/div[2]/div[3]/button[3]"
+    Cookie_einstellungen_XPATH='//*[@id="cookie-consent"]/div[2]/div[3]/button[1]/span'
+        
+    Cookie_einstellungen_CSS_PATH='#cookie-consent > div.bg-secondary-background-modal.sm\:container.p-3.md\:\!p-12.text-white.m-1.border.border-white > div.sticky.bottom-0 > button.mt-4.p-2.font-semibold.w-full.inline-flex.justify-center.items-center.text-white.border.border-white.bg-trend-positive.fill-white > span'
+
+    Cookie_einstellungen_CLASS="mt-4 p-2 font-semibold w-full inline-flex justify-center items-center text-white border border-white bg-trend-positive fill-white"
+    Cookie_einstellungen_SELECTOR="#cookie-consent > div.bg-secondary-background-modal.sm\:container.p-3.md\:\!p-12.text-white.m-1.border.border-white > div.sticky.bottom-0 > button.mt-2\.5.p-2.font-semibold.w-full.transition.inline-flex.justify-center.items-center.text-white.border.border-white.hover\:bg-white.hover\:text-primary-dark.hover\:border-primary-dark"
+
+    Wahrung_click_id_1="mainCurrencyAmountPrincipal"
+    
     Wahrung_click_id="bsg-filters-btn-bgs-filter-7"
     Wahrung_click_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[6]/div/button"
     Wahrung_click_new_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[6]/div/div/div/div[1]/div/div[1]/ul/li[3]/div/div/input"
+    Wahrung_click_new_full_XPATH="/html/body/div[1]/main/div[1]/div/div/div[2]/div/div[1]/div[7]/div/button"
+
     Wahrung_click_new_ID="bsg-checkbox-1576"
     Wahrung_write_field_css="#bsg-filters-menu-bgs-filter-7 > div > div.bsg-dropdown__search > div > input"
     Wahrung_write_field_full_xp="/html/body/main/div[2]/div/div/div/form/div/div[2]/ul/li[7]/div/button"
+    Wahrung_write_field_full_1_xp="/html/body/div[1]/main/div[1]/div/div/div[2]/div/div[1]/div[7]/div/div/div[1]/div/input"
+    Wahrung_write_field_full_2_xp="/html/body/div[1]/main/div[1]/div/div/div[2]/div/div[1]/div[7]/div/div/div[1]/div/input"
+    Wahrung_write_field_full_3_xp="/html/body/div[1]/main/div[1]/div/div/div[2]/div/div[1]/div[7]/div/div/div[1]"
+    Wahrung_write_field_full_4_xp="#mainCurrencyAmountPrincipal > div > div > div:nth-child(1) > div > input"
+    Wahrung_write_field_class="w-full relative border border-solid border-primary-dark"
     Wahrung_write_field_simpl_xp="//*[@id='bsg-filters-btn-bgs-filter-7']"
+
+    Wahrung_Scroll_list_xp="/html/body/div[1]/main/div[1]/div/div/div[2]/div/div[1]/div[7]/div/div/ul"
+    Wahrung_click_CSS_SELECTOR="#mainCurrencyAmountPrincipal > div > button"
+    Wahrung_click_XPATH='//*[@id="mainCurrencyAmountPrincipal"]/div/button'
+
+    Wahrung_text_field_click_CSS_1_SELECTOR="#mainCurrencyAmountPrincipal > div > div > div:nth-child(1) > div > input"
+    Wahrung_text_field_click_CSS_2_SELECTOR="#mainCurrencyAmountPrincipal > div > div > div:nth-child(1) > div"
+    Wahrung_text_field_click_CSS_3_SELECTOR="#mainCurrencyAmountPrincipal > div > div > div:nth-child(1) > div > input"
+    Wahrung_text_field_click_CSS_4_SELECTOR="#mainCurrencyAmountPrincipal > div > div > div:nth-child(1) > div > button > i"
+    Wahrung_text_field_click_XPATH='//*[@id="mainCurrencyAmountPrincipal"]/div/div/div[1]/div/button'
+    
+    Wahrung_suurennuslasi_full_XP="/html/body/div[1]/main/div[1]/div/div/div[2]/div/div[1]/div[7]/div/div/div[1]/div/button/i"
     Wahrung_checkbox_css="#bsg-checkbox-1571"
     Wahrung_checkbox_Euro_id="bsg-checkbox-1569"
     Wahrung_checkbox_Euro_full_XPath="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[6]/div/div/div/div[2]/div/div[1]/ul/li/div/div/label"
+
+    Wahrung_checkbox_Euro_XPath='//*[@id="mainCurrencyAmountPrincipal"]/div/div/div[2]/button/span'
+    Wahrung_checkbox_Eur_CSS="#mainCurrencyAmountPrincipal > div > div > div.w-full.p-2 > button"
+
+    
+    Wahrung_Euro_small_cross_click_XP='//*[@id="mainCurrencyAmountPrincipal"]/div/div/ul/li/div/div/span/label'
+    Wahrung_Anwenden_XP='//*[@id="mainCurrencyAmountPrincipal"]/div/div/div[2]/button/span'
+
+    
+
     #Wahrung_checkbox_Euro_Xpath="//*[@id="mCSB_4_container"]/ul/li/div/div/label"
     Wahrung_checkbox_Euro_full_Xpath="/html/body/main/div[2]/div/div/div/form/div/div[2]/ul/li[6]/div/div/div/div[2]/div/div[1]/ul/li/div/div/label"
     Wahrung_anwenden_css="#bsg-filters-menu-bgs-filter-7 > div > div.bsg-dropdown__footer > button"
     Zusatzliche_Filter_Textfield_Wahrung_Full_XPath="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[6]/div/div/div/div[1]/div/input"
 
+    Zusatzliche_Filter_simple_XPATH='//*[@id="additional-filters"]/div/button/span'
+
+    Zusatzliche_Filter_right_XPATH='//*[@id="additional-filters"]/div/button'
+    Zusatzliche_Filter_full_XPATH="/html/body/div[3]/main/div[2]/div/div[2]/div/button/span"
+
+    Zusatzliche_Filter_CSS_selector='#additional-filters > div > button > span'
+
+
+    Zusatzliche_Filter_plus_selection_button_Full_XPath_later="/html/body/div[1]/main/div[1]/div/div/div[2]/div/div[2]/div/button"
+    Zusatzliche_Filter_Full_XPath='//*[@id="additional-filters"]/div/button/span'
+
+    Zusatzliche_Filter_Textfield_Wahrung_XPath="//*[@id='mainCurrencyAmountPrincipal']/div/button"
+
+
+    Zusatzliche_Filter_Textfield_Wahrung_classname="p-2 font-semibold border w-full text-primary-dark border-primary-dark justify-between disabled:opacity-75 disabled:pointer-events-none transition hover:bg-primary-dark hover:text-white fill-primary-dark hover:fill-white inline-flex text-left items-center"
+    Zusatzliche_Filter_Textfield_Wahrung_classname_1="w-full text-secondary-gray ring-0 focus:ring-0 border-0"
+    Zusatzliche_Filter_click_Wahrung_ID="mainCurrencyAmountPrincipal"
+    Zusatzliche_Filter_click_Wahrung_full_XPATH="/html/body/div[1]/main/div[1]/div/div/div[2]/div/div[1]/div[7]"
     #Zusatzliche_Filter_css="#bsg-filters-btn-bgs-filter-8"
-    Zusatzliche_Filter_css="#bsg-filters-btn-bgs-filter-11"
-    handelbare_Einh_css="html.tablesaw-enhanced body.bsg-page main.bsg-main.bsg-page__main div.bsg-searchlist div.bsg-searchlist__header.bsg-ribbon.bsg-theme--cira div.bsg-container div.bsg-row.bsg-ribbon__row form.bsg-searchlist__form.js-bsg-searchlist div.bsg-searchlist__form-items-wrapper div.bsg-searchlist__dynamic-filters ul.bsg-filter-group li.bsg-filter-group__item.bsg-filter-group__item--fluid div.bsg-dropdown.bsg-dropdown--gina.bsg-filter.js-bsg-filter div#bsg-filters-menu-bgs-filter-8.bsg-dropdown__menu.bsg-filter__panel.bsg-filter__panel--gina div.bsg-dropdown__block div.bsg-scrollbox.js-bsg-scrollbox.bsg-dropdown__scrollbox.mCustomScrollbar._mCS_5 div#mCSB_5.mCustomScrollBox.mCS-bsg-dropdown-default.mCSB_vertical.mCSB_inside div#mCSB_5_container.mCSB_container ul.bsg-dropdown__list.bsg-dropdown__list--shorten li.bsg-dropdown__item div.bsg-form-controlbox.bsg-form-controlbox--custom.bsg-form-controlbox--leia div input#bsg-checkbox-11.bsg-form-controlbox__control"
+    Zusatzliche_Filter_css='#additional-filters > div > button > span'
+
+
+    Zusatzliche_Filter_full_XP="/html/body/div[1]/main/div[1]/div/div/div[2]/div/div[2]/div/button/span"
+    handelbare_Einh_css='#additional-filters > div > div > ul > li:nth-child(3) > div > div > span > label'
+
+    Handelbare_einh_Button_CSS='//*[@id="minOrderVolume"]/div/button/span'
     #new 2023
     Handelbare_einh_Button_full_xpath="/html/body/main/div[2]/div/div/div/form/div/div[3]/ul/li/div/div/div/div[2]/div/div[1]/ul/li[3]/div/div/input"
-    Handelbare_einh_Button_xpath="//*[@id='bsg-checkbox-14']"
+    Handelbare_einh_Button_xpath='//*[@id="minOrderVolume"]/div/button/span'
     Handelbare_einh_Button_id="bsg-checkbox-14"
     Handelbare_einh_Button_class_1="bsg-form-controlbox__control"
+    Einloggen_FULL_XPATH="/html/body/div[1]/main/div[1]/div/div/div[1]/div/div/div[2]/div/div/div/p[2]/a[1]"    
     
     Zusatzliche_Filter_Textfield_Full_XPath="/html/body/main/div[2]/div/div/div/form/div/div[3]/ul/li/div/div/div/div[1]/div/input"
-    Zusatzliche_Filter_Anwenden_Full_XPath="/html/body/main/div[2]/div/div/div/form/div/div[3]/ul/li/div/div/div/div[3]/button"
-    Zusatzliche_Filter_Full_XPath="/html/body/main/div[2]/div[1]/div/div/form/div/div[3]/ul/li/div/button"
-    
-    
+    Zusatzliche_Filter_Textfield_Full_XPath_updated="/html/body/div[1]/main/div[1]/div/div/div[2]/div/div[2]/div/button"
+
+    yellow_upper_row__disturbing_click_away_full_XPath="/html/body/div[1]/header/div/div/div[2]"
+    yellow_upper_row__disturbing_click_away_XPath="//*[@id='header']/div/div/div[2]/button"
+    Zusatzliche_Filter_Anwenden_Full_XPath='//*[@id="additional-filters"]/div/div/div/button'
+    Zusatzliche_Filter_Anwenden_CSS='#additional-filters > div > div > div > button'
+    Zusatzliche_Filter_normal_XPath="//*[@id='additional-filters']/div/button"
+    Zusatzliche_Filter_normal_XPath="//*[@id='additional-filters']/div/button"
+    yellow_upper_row__disturbing_click_away_css_TÄ="button.text-5 i.icon-outline-close.text-5.text-primary-dark"
+
     Handelbare_Einheit_class="bsg-btn bsg-btn--dropdown bsg-btn--nhs bsg-dropdown__btn bsg-filter__control bsg-filter__control--juna"
     Anwenden_button_css="#bsg-filters-menu-bgs-filter-8 > div > div.bsg-dropdown__footer > button"
     Handelbare_einh_Button_class='bsg-btn bsg-btn--dropdown bsg-btn--nhs bsg-dropdown__btn bsg-filter__control bsg-filter__control--juna'
     Handelbare_einh_Button_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[7]/div/button"
+    Handelbare_einh_Button_xp_later="/html/body/div[1]/main/div[1]/div/div/div[2]/div/div[2]/div/div/ul/li[2]/div/div/span/input"
+    Gelisteter_Zeitraum_Button_xpath_1='//*[@id="additional-filters"]/div/div/ul/li[3]/div/div/span'
     right_arrow_full_xp="/html/body/main/div[2]/div[3]/div/div/div[1]/div/div[1]/a[2]"
     right_arrow_class="btn tablesaw-nav-btn tablesaw-btn btn-micro right"
 
-    min_Littera_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[7]/div/div/div/div[2]/div[1]/input"
-    max_Littera_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[7]/div/div/div/div[2]/div[2]/input"
-    Anwenden_littera_xp ="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[7]/div/div/div/div[4]/button"
-    Anwenden_littera_css="#bsg-filters-menu-bgs-range-filter-1594 > div > div.bsg-dropdown__footer > button"
+    min_Littera_xp='//*[@id="minOrderVolume"]/div/div/div/div[2]/div[1]/input'
+    max_Littera_xp='//*[@id="minOrderVolume"]/div/div/div/div[2]/div[2]/input'
+    Anwenden_littera_xp ='//*[@id="minOrderVolume"]/div/div/div/button'
+    Anwenden_littera_css="#minOrderVolume > div > div > div > button"
 
     Delete_littera_full_Xpath="/html/body/main/div[2]/div[2]/div/div/ul/li/button"
     #                          /html/body/main/div[2]/div[2]/div/div/ul/li/button
@@ -51,12 +128,29 @@ class Locators():
    # Anwenden_littera_full_XPath="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[7]/div/div/div/div[4]/button"
     Anwenden_littera_full_XPath="/html/body/main/div[2]/div/div/div/form/div/div[2]/ul/li[7]/div/div/div/div[4]/button"
     
-    Falligkeit_click_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[4]/div/button"
-    Smaller_date_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[4]/div/div/div/div[2]/div[1]/input"
+    Smaller_date_xp='/html/body/div[3]/main/div[2]/div/div[1]/div[5]/div/div[1]/div/div/input[1]'
 
     Smaller_date_ID="bsg-date-range-input-1623"
-    Bigger_date_xp= "/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[4]/div/div/div/div[2]/div[2]/label"
-    Bigger_date_2_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[4]/div/div/div/div[2]/div[2]/input"
+    Smaller_date_2_xp= '/html/body/div[3]/main/div[2]/div/div[1]/div[5]/div/div[1]/div/div/input[1]'
+    Bigger_date_2_xp="/html/body/div[3]/main/div[2]/div/div[1]/div[5]/div/div[1]/div/div/input[2]"
+    Small_calendersymbol_full_xp="/html/body/div[3]/main/div[2]/div/div[1]/div[5]/div/div[1]/div/div/div[1]/button/span/svg"
+    Bigger_date_2_css='[id^="picker"] > div.mud-input-control.mud-input-control-margin-dense > div > div > input:nth-child(3)'
+    Bigger_date_full_xp="/html/body/div[3]/main/div[2]/div/div[1]/div[10]/div/div/div/div[2]/div[2]/input"
+    
+    
+    Gelisteter_Zeitraum_ruksi_css="#additional-filters > div > div > ul > li:nth-child(4) > div > div > span > label"
+
+    Anwenden_Gelisteter_Zeitraum_css="#additional-filters > div > div > div > button > span"
+    Anwenden_Gelisteter_Zeitraum_XPATH='//*[@id="additional-filters"]/div/div/div/button'
+    
+
+    faelligkeit_element_bigger_date_FULL_XPATH="/html/body/div[3]/main/div[2]/div/div[1]/div[5]/div/div[1]/div/div/input[2]"
+
+    faelligkeit_element_bigger_date_XPATH='//div[contains(@id, "picker")]/div[1]/div/div/input[2]'
+
+    gelistet_zeitraum_element_smaller_Date_XPATH='//input[@placeholder="Gelistet Zeitraum"]'
+
+    gelistet_zeitraun_small_date_css='[id^="picker"] > div.mud-input-control.mud-input-control-margin-dense > div > div > input:nth-child(1)'
     Anwenden_Falligkeit_full_XPath="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[4]/div/div/div/div[4]/button"
     Rendite_button_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[5]/div/button"
     Rendite_lower_margin_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[5]/div/div/div/div[2]/div[1]/input"
@@ -74,18 +168,49 @@ class Locators():
     Gelisteter_Zeitraum_preselector_Button_id="bsg-checkbox-1612"
     Gelisteter_Zeitraum_Button_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[8]/div/button"
     Gelisteter_Zeitraum_smaller_date_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[8]/div/div/div/div[2]/div[1]/input"
-                                         
+    Date_field_ID="dateMaturity"     
+    Date_field_XPATH='//*[@id="dateMaturity"]'
+    Date_field_CSS='#dateMaturity'
+
     Gelisteter_Zeitraum_smaller_date_ID="Gelisteter_Zeitraum_smaller_date_ID"
     Gelisteter_Zeitraum_Bigger_date_xp="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[8]/div/div/div/div[2]/div[2]/input"
     Anwenden_Gelisteter_Zeitraum_full_XPath="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[8]/div/div/div/div[4]/button"
     
     #For Type of Bond
+
+    Preselector_Anleihen_Typ_CSS='#categoryLevel2 > div > button' 
+    Preselector_Anleihen_Typ_XPATH='//*[@id="categoryLevel2"]/div/button'
+    Preselector_Anleihen_Typ_FULL_XPATH="/html/body/div[3]/main/div[2]/div/div[1]/div[2]/div/button"
+    Unternehmensanleihe_textfield_XPATH='//*[@id="categoryLevel2"]/div/div/div[1]/div/input'
+    Unternehmensanleihe_textfield_CSS='#categoryLevel2 > div > div > div:nth-child(1) > div'
+    Unternehmensanleihe_checkbox_ID="fc045af3-8bca-4043-aa52-3bf814e30097"
+    Unternehmensanleihe_checkbox_XPATH='//*[@id="fc045af3-8bca-4043-aa52-3bf814e30097"]'
+    Unternehmensanleihe_checkbox_PARTIAL_FULL_XPATH="//ul/li[5]/div/div/span/input"
     Preselector_Anleihen_Typ_ID="bsg-filters-btn-bgs-filter-3"
+
+    Unternehmensanleihe_checkbox_with_text_XPATH='XPATH=//*[@id="categoryLevel2"]/div/div/ul/li/div/div/span/label'
     Preselector_Anleihen_Typ_Full_XPath="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[2]/div/button"
     Select_Unternehmensanleihe_Checkbox_FullXPath="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[2]/div/div/div/div[1]/div/div[1]/ul/li[4]/div/div/input"
     Select_Unternehmensanleihe_Checkbox_ID="bsg-checkbox-1536"
     Anwenden_Unternehmensanleihe_Select_full_XPath="/html/body/main/div[2]/div[1]/div/div/form/div/div[2]/ul/li[2]/div/div/div/div[2]/button"
-    Anwenden_Unternehmensanleihe_Select_class="bsg-btn bsg-btn--juna bsg-btn--nes bsg-btn--centered bsg-dropdown__btn-apply js-bsg-filter__closer"
-    
+ 
+    Anwenden_Unternehmensanleihe_Select_class="#categoryLevel2 > div > div > div.w-full.p-2 > button"
 
 
+    Anwenden_Unternehmensanleihe_XPATH='//*[@id="categoryLevel2"]/div/div/div[2]/button'
+
+    Search_engine_radiobutton_Full_XPath="/html/body/search-engine-choice-app//div[1]/cr-radio-group/cr-radio-button[7]//div[1]/cr-ripple"
+    Search_engine_radiobutton_bigger_Full_XPath="/html/body/search-engine-choice-app//div[1]/cr-radio-group/cr-radio-button[1]//div[1]/div[1]"
+    Confirm__search_engine_radiobutton_Full_XPath="/html/body/search-engine-choice-app//div[2]/cr-button"
+
+    Rendite_button_XPATH='//*[@id="yieldToMaturity"]/div/button'
+    Rendite_textbox_CSS='#yieldToMaturity > div > div > div > div.grid.grid-cols-2.gap-2 > div:nth-child(1) > input'
+    Rendite_textbox_XPATH='//*[@id="yieldToMaturity"]/div/div/div/div[2]/div[1]/input'
+    Rendite_textbox_FULLXPATH='/html/body/div[3]/main/div[2]/div/div[1]/div[6]/div/div/div/div[2]/div[1]/input'
+
+    Rendite_Anwenden_CSS='#yieldToMaturity > div > div > div > button > span'
+    Rendite_Anwenden_XPATH='//*[@id="yieldToMaturity"]/div/div/div/button/span'
+    Rendite_Anwenden_FULLXPATH='/html/body/div[3]/main/div[2]/div/div[1]/div[6]/div/div/div/button/span'
+    Weitere_Treffer_Anzeigen_CSS='#ProductsFinderBlock-48fe4311-4bd6-4736-bfcf-6903a551734c > div > div:nth-child(4) > div.w-fit > button'
+    Weitere_Treffer_Anzeigen_XPATH='//*[@id="ProductsFinderBlock-48fe4311-4bd6-4736-bfcf-6903a551734c"]/div/div[4]/div[3]/button'
+    Weitere_Treffer_Anzeigen_FULL_XPATH='/html/body/div[3]/main/div[2]/div/div[4]/div[3]/button'
